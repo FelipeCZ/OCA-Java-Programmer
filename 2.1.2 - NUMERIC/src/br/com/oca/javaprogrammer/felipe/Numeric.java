f96441f999e4fcf6d94431b66340e17b3dc06093
FelipeCZ <flp313@gmail.com>
2015-05-31 15:03:26 -0300

    Underscores do not change the decimal values. 100==1_0_0

---
+++ 2.1.2 - NUMERIC/src/br/com/oca/javaprogrammer/felipe/Numeric.java
@@ -0,0 +1,13 @@
+package br.com.oca.javaprogrammer.felipe;
+
+public class Numeric {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=1_0_0;
+		if(a==b){
+			System.out.println("a and b are equal!!!");
+		}
+	}
+
+}
