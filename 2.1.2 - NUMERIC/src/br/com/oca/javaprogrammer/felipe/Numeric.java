c3f621d247b44f30e32e82d72737eade8fc67ed7
FelipeCZ <flp313@gmail.com>
2015-06-12 00:51:09 -0300

    .

--- 2.1.2 - NUMERIC/src/br/com/oca/javaprogrammer/felipe/Numeric.java
+++ 2.1.2 - NUMERIC/src/br/com/oca/javaprogrammer/felipe/Numeric.java
@@ -2,7 +2,7 @@ package br.com.oca.javaprogrammer.felipe;
 
 public class Numeric {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		int a=100;
 		int b=1_0_0;
 		if(a==b){
